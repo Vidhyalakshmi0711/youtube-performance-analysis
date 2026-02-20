7256040a218c3a0b01d176e09bfb8390a6446124
Vidhya-data-analyst <vidhyalakshmi@aadhan.in>
2026-02-20 16:41:33 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,45 +1,7 @@
 # db.py
-import psycopg2
-import pandas as pd
-from contextlib import contextmanager
-from config import DB_CONFIG
+from config import conn
 
-
-# -----------------------------
-# Connection manager
-# -----------------------------
-@contextmanager
-def get_conn():
-    conn = psycopg2.connect(**DB_CONFIG)
-    try:
-        yield conn
-    finally:
-        conn.close()
-
-
-# -----------------------------
-# Fetch data as Pandas DataFrame (for dashboard)
-# -----------------------------
-def fetch_df(query, params=None):
-    with get_conn() as conn:
-        return pd.read_sql(query, conn, params=params)
-
-
-# -----------------------------
-# Fetch raw rows (for pipelines)
-# -----------------------------
-def fetch_all(query, params=None):
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute(query, params or ())
-            return cur.fetchall()
-
-
-# -----------------------------
-# Execute INSERT / UPDATE / DELETE
-# -----------------------------
-def execute(query, params=None):
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute(query, params or ())
-            conn.commit()
+def get_data(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
