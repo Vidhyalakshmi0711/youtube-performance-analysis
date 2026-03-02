88d760a9d047a63be36f65d5a1277ce1da07bc6f
Vidhya-data-analyst <vidhyalakshmi@aadhan.in>
2026-03-02 10:49:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-# config.py
-import streamlit as st
+import os
 import psycopg2
 
 # Database connection
 conn = psycopg2.connect(
-    host=st.secrets["DB_HOST"],
-    database=st.secrets["DB_NAME"],
-    user=st.secrets["DB_USER"],
-    password=st.secrets["DB_PASSWORD"],
-    port=st.secrets["DB_PORT"],
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT"),
     sslmode="require"
 )
 
 # YouTube API key
-API_KEY = st.secrets["YOUTUBE_API_KEY"]
+API_KEY = os.getenv("YOUTUBE_API_KEY")
