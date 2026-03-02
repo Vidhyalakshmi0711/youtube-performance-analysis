22fee5de327bbfc89ba8198b7d819fe6b4648a70
Vidhya-data-analyst <vidhyalakshmi@aadhan.in>
2026-03-02 11:25:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+import os
+import psycopg2
+
+# Database connection
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT"),
+    sslmode="require"
+)
+
+# YouTube API key
+API_KEY = os.getenv("YOUTUBE_API_KEY")
